ee617bccd2a3b3054bd0d21395c8e31f1a415c29
Konrad Jaowiecki <Dexter@localhost.localdomain>
2013-12-28 20:25:00 +0100

    Rearranged core and fir_filters subpackages. Moved all functionallity of core module into core.py file. Moved all functionallity of subpackage fir_filters into fir_filters.py file. Deleted now unused directories core/ and fir_filters/. fir_filter.py. Removed now unused directory fir_filters/.

--- source/asc-0.1/src/asc/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from time_series import TimeSeries
-from histogram import Histogram
-
-del time_series
-del histogram
--- source/asc-0.1/src/asc/fir_filters/__init__.py
+++
@@ -1,5 +0,0 @@
-from fir_filter import firFilter
-from moving_average import MA
-
-del fir_filter
-del moving_average
